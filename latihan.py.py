7d7b07b8b8a67ba51638f12437d419b784ffdb2f
Ilham kusuma kaya <102157631+Ilhampmb114@users.noreply.github.com>
2022-03-24 08:02:31 +0700

    Add files via upload

---
+++ latihan.py.py
@@ -0,0 +1,14 @@
+uas = int(input("nilai uas"))
+uts = int(input("nilai uts"))
+pratikum = int(input("nilai pratikum"))
+
+rata(uas+uts+pratikum/3)
+print("================")
+
+print ("nilai rata-rata ",rata)
+nilai_tertinggi =65
+print ("anda dinyatakan")
+if rata >= nilai_tertiggi:
+   print ("lulus")
+else:
+    print ("anda belum lulus lulus")
